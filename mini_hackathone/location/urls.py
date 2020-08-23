f18503b270ec15a5833bd91d00464f328405b5d2
peanutyumyum <jrimit@gmail.com>
2020-08-23 22:58:02 +0900

    sketch location app

--- mini_hackathone/location/urls.py
+++ mini_hackathone/location/urls.py
@@ -5,5 +5,6 @@ app_name = 'location'
 
 urlpatterns=[
     path('location/', views.location, name="location"),
-    path('location/<str:city_name>', views.location_filtered_bookstore, name="bookstore"),
-]
+    path('location/<str:city_name>', views.location_filtered_city_scale, name="location_filtered_city_scale"),
+    path('location/<str:town_name>', views.location_filtered_bookstore, name="location_filtered_bookstore"),
+    path('location/<str:bookstore_name>', views.location_bookstore_specific_information, name="location_bookstore_specific_information"),
--- mini_hackathone/location/views.py
+++ mini_hackathone/location/views.py
@@ -20,8 +20,8 @@ def location(request):
     
     return render(request, 'location.html', context)
 
-def location_filtered_bookstore(request, city_name):
-    filtered_bookstore_model = Bookstore.objects.filter(city_address_of_bookstore=city_name)
+def location_filtered_city_scale(request, city_name):
+    filtered_bookstore_model = Bookstore.objects.filter(city_address_of_bookstore=city_name) # city_name was delivered with string form through url, so you don't need to write '' formation.
     # insert model information to use in location_filtered_bookstore.html
 
 
@@ -30,4 +30,10 @@ def location_filtered_bookstore(request, city_name):
     }
     # declare variable
 
-    return render(request, 'location_filtered_bookstore.html', context)
+    return render(request, 'location_filtered_city_scale.html', context)
+
+def location_filtered_bookstore(request, town_name):
+    return render(request, 'location_filtered_bookstore.html')
+
+def location_bookstore_specific_information(request, bookstore_name):
+    return render(request, 'location_bookstore_specific_information.html')
